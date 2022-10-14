464693cb52667398270621064ffd31864b0d1bb4
marOne-mrri <hello@mar0ne.com>
2022-10-14 16:39:47 +0100

    issue with strings

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::io::{stdin, stdout, BufWriter};
+use rand::Rng;
 
 fn main() {
+    let secret = rand::thread_rng().gen_range(1..100);
+    let str_secret = secret.to_string();
+    print!("enter something {}", secret);
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("failed to read");
     let stdout = stdout();
-    let message = String::from("Hello, World!!");
+    let msg = input.push_str(&str_secret);
+    let message = String::from(msg); // ! prb
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
